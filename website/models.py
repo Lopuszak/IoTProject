b533d35ce2fbb22d6c7083101f5a9558f721e68b
Szymek Ł <shibek123@gmail.com>
2023-01-28 20:47:03 +0100

    Employee edit and logs search

--- website/models.py
+++ website/models.py
@@ -6,6 +6,9 @@ class Employee(db.Model):
     card_id = db.Column(db.String(20), primary_key=True, autoincrement=True)
     name = db.Column(db.String(100))
 
+    def __str__(self):
+        return "Employee: %s id: %s" % (self.name, self.card_id)
+
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     card_id = db.Column(db.String(20), db.ForeignKey('employee.card_id'))
--- website/views.py
+++ website/views.py
@@ -1,11 +1,28 @@
 import json
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, redirect, render_template, request, flash, jsonify, url_for
 from .models import *
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET'])
+@views.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        print(request.form)
+        if "search_id" in request.form:
+            id = request.form['search_id']
+            return redirect('/logs/'+str(id))
+        else:
+            new_name = request.form['employee_name']
+            print(new_name)
+            employee_id = request.form['employee_id']
+            if len(new_name) < 1 or new_name is None:
+                flash('Name is too short!', category='error')
+            else:
+
+                employee = Employee.query.filter_by(card_id=employee_id).first()
+                employee.name = new_name
+                db.session.commit()
+
     employee_list = Employee.query.all()
     return render_template('index.html', employee_list = employee_list)
 
@@ -22,25 +39,25 @@ def group_logs():
     return render_template('group_logs.html', logs=logs_dict)
 
 
-@views.route('/logs/<card_id>')
-def logs():
-    card_id = request.args.get('card_id')
-    employee = Employee.query.filter_by(card_id=card_id).first()
+@views.route('/logs/<employee_id>')
+def logs(employee_id):
+    employee = Employee.query.filter_by(card_id=employee_id).first()
     if employee:
-        logs = Log.query.filter_by(card_id=card_id).all()
+        logs = Log.query.filter_by(card_id=employee_id).all()
         return render_template('logs.html', logs=logs)
     else:
         return 'Employee not found'
 
 @views.route('/logs/avg/<card_id>')
-def avg():
+def avg(card_id):
     card_id = request.args.get('card_id')
+    card_id = '<Employee '+str(card_id)+'>'
     employee = Employee.query.filter_by(card_id=card_id).first()
     if employee:
         logs = Log.query.filter_by(card_id=card_id).all()
 
         
 
-        return render_template('avgs.html', logs=logs)
+        return render_template('avg.html', logs=logs)
     else:
         return 'Employee not found'
